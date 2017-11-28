f819e5bc2ae8f9d98cc0fb725ce5373fb05ee8ea
Jeremy Gibson <jeremygibson@jeremys-mbp.lan>
2017-11-28 14:50:05 -0500

    darcm_server now working on MacOS

--- DarcMailCLI.py
+++ DarcMailCLI.py
@@ -375,10 +375,12 @@ class ValidateStructure(object):
 
 class BuildEmlDarcmail(object):
     def __init__(self, darcmail):
+        self.logger = logging.getLogger("Main_Eml")
         self.account_directory = darcmail.account_directory
         self.account_name = darcmail.account_name
         self.xml_dir = darcmail.xml_dir
         emwalk = EmlWalker(self.account_directory, self.xml_dir, self.account_name)
+        self.logger.info("Processing {} \n to {}".format(self.account_directory, self.xml_dir))
         emwalk.do_walk()
 
 
