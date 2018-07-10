f24700e7af932d0160c4a4f3725d4c3e7cbe16a7
Nitaro <nitaro74@gmail.com>
2018-07-10 11:12:19 -0400

    Added skeleton unittest.

---
+++ tests/test__DarcMailCLI.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# import modules.
+import logging
+import os
+import requests
+import unittest
+from lxml import etree
+
+# enable logging.
+logging.basicConfig(level=logging.DEBUG)
+
+
+# load EAXS XSD.
+XSD = requests.get("https://raw.githubusercontent.com/StateArchivesOfNorthCarolina/tomes-eaxs/master/mail-account.xsd").text.encode()
+
+
+class Test_DarcMailCLI(unittest.TestCase):
+
+
+    def setUp(self):
+
+        # set attributes.
+        self.sample_folder = "sample_files"
+        self.xsd = XSD
+        self.validator = etree.XMLSchema(etree.fromstring(self.xsd))
+
+
+    def test__mbox(self):
+        """ ??? """
+        
+        s = etree.parse("../OUTPUT/eaxs/sampleml/eaxs_xml/sampleml.xml_LID_0.xml")
+        return self.validator.validate(s)
+        
+
+    def test__eml(self):
+        """ ??? """
+
+        return True
