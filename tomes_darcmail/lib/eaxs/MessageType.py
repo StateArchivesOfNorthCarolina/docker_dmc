120eb8d6f435e17290e21f7ad70612564d5b77a3
Nitaro <nitaro74@gmail.com>
2018-08-09 14:08:56 -0400

    Removed TODO.

--- tomes_darcmail/lib/eaxs/MessageType.py
+++ tomes_darcmail/lib/eaxs/MessageType.py
@@ -33,7 +33,7 @@ class DmMessage:
         self.message = message  # type: Message
         self.fn = fn
         # First parts of the schema message-type
-        self.relative_path = rel_path #TODO NITIN: .replace("\\", "/").replace("//", "/")  # type: str
+        self.relative_path = rel_path
         self.local_id = local_id
         self.message_id = CommonMethods.cdata_wrap(self.message.get("Message-ID"))  # type: str
         if self.message_id == '' or self.message_id is None:
