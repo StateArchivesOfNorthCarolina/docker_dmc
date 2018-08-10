4721ff32b2a58a89791cb46312c62a4acbabe7ae
Nitaro <nitaro74@gmail.com>
2018-08-09 20:55:17 -0400

    Improved unit test.

--- tests/test__darcmail.py
+++ tests/test__darcmail.py
@@ -15,21 +15,27 @@ from tomes_darcmail.darcmail import DarcMail
 logging.basicConfig(level=logging.DEBUG)
 
 
-# set path and XSD data.
+# set global path and validator.
 HERE = os.path.dirname(__file__)
-XSD_PATH = "https://raw.githubusercontent.com/StateArchivesOfNorthCarolina/tomes-eaxs/master/versions/1/eaxs_schema_v1.xsd"
-XSD = requests.get(XSD_PATH).text.encode()
-XSD_EL = etree.fromstring(XSD)
-VALIDATOR = etree.XMLSchema(XSD_EL)
+VALIDATOR = []
 
 
 def VALIDATE_EAXS(account_id, sample_data, is_eml=False):
     """ Creates a temporary EAXS with name @account_id using @sample_data.
     
     Returns:
-        bool: The return value.
-        True if the EAXS is valid per @XSD_PATH. Otherwise, False. """
+        tuple: The return value.
+        The first item is a boolean that is True if the EAXS is valid. 
+        The second item is the EAXS file as an lxml.etree._Element. 
+    """
 
+    # set XSD data and prevent repeat http requests.
+    if len(VALIDATOR) == 0:
+        xsd_path = "https://raw.githubusercontent.com/StateArchivesOfNorthCarolina/tomes-eaxs/master/versions/1/eaxs_schema_v1.xsd"
+        xsd = requests.get(xsd_path).text.encode()
+        xsd_el = etree.fromstring(xsd)
+        VALIDATOR.append(etree.XMLSchema(xsd_el))
+        
     # create temporary folder.
     temp_dir = tempfile.TemporaryDirectory(dir=HERE)
     
@@ -43,11 +49,11 @@ def VALIDATE_EAXS(account_id, sample_data, is_eml=False):
     # validate EAXS.
     logging.info("Validating EAXS at: {}".format(eaxs))
     eaxs_el = etree.parse(eaxs)
-    is_valid = VALIDATOR.validate(eaxs_el)
+    is_valid = VALIDATOR[0].validate(eaxs_el)
 
     temp_dir.cleanup()
     logging.info("XML validation yielded: {}".format(is_valid))
-    return is_valid
+    return (is_valid, eaxs_el)
 
 
 class Test_DarcMail(unittest.TestCase):
@@ -56,27 +62,23 @@ class Test_DarcMail(unittest.TestCase):
     def setUp(self):
 
         # set attributes.
-        self.here = HERE 
+        self.here = HERE
+        self.validate_eaxs = VALIDATE_EAXS
         self.sample_mbox = os.path.join(self.here, "sample_files", "mbox")
         self.sample_eml = os.path.join(self.here, "sample_files", "eml")
-        self.xsd_path = XSD_PATH
-        self.xsd = XSD
-        self.xsd_el = XSD_EL
-        self.validator = VALIDATOR
-        self._validate_EAXS = VALIDATE_EAXS
 
 
     def test__mbox(self):
         """ Does the sample MBOX yield a valid EAXS? """
         
-        is_valid = self._validate_EAXS("sample_mbox", self.sample_mbox) 
+        is_valid, eaxs_el = self.validate_eaxs("sample_mbox", self.sample_mbox) 
         self.assertTrue(is_valid)
   
 
     def test__eml(self):
         """ Does the sample EML yield a valid EAXS? """
 
-        is_valid = self._validate_EAXS("sample_eml", self.sample_eml, is_eml=True) 
+        is_valid, eaxs_el = self.validate_eaxs("sample_eml", self.sample_eml, is_eml=True) 
         self.assertTrue(is_valid)
 
 
@@ -84,14 +86,22 @@ class Test_DarcMail(unittest.TestCase):
 def main(account_path: "email account path", 
         from_eml: ("toggle EML processing", "flag", "fe")):
     
-    "Creates and validates an EAXS account.\
-    \nWARNING: Only use this with *small* MBOX|EML data (< 100 messages).\
-    \nexample: `python3 test__darcmail sample_files/mbox`"
+    "Creates an EAXS account and prints basic account statistics.\
+    \nexample: `python3 test__darcmail sample_files/mbox`\
+    \n\nWARNING: Only use this with *small* MBOX|EML data (< 100 messages)."
 
     # creates EAXS.
-    is_valid = VALIDATE_EAXS("sample", account_path, is_eml=from_eml)
-    print(is_valid)
+    is_valid, eaxs_el = VALIDATE_EAXS("sample", account_path, is_eml=from_eml)
+    
+    # get folder and message statistics.
+    folders = eaxs_el.xpath("//*[local-name()='Folder']")
+    messages = eaxs_el.xpath("//*[local-name()='Message']")
+    
+    # report.
+    stats = "EAXS had {} total folders with {} total messages.".format(len(folders), 
+            len(messages))
+    print(stats)
 
-
+    
 if __name__ == "__main__":
     plac.call(main)
